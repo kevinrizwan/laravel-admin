ce4e8d261a0f73c0277afad4dd38fed8d036f90e
kevinrizwan <kevin.lyoko01@gmail.com>
2021-03-31 06:56:51 +0700

    templateUIadmin

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'admin'], function (){
+    Route::get('/', 'Admin\DashboardController@index');
+});
