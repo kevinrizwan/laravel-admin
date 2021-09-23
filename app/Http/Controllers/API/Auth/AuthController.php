8faa5bf94be6f153804c2e0491833df108fb2c71
kevinrizwan <kevin.lyoko01@gmail.com>
2021-09-23 22:20:54 +0700

    add auth register

---
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:25',
+            'email' => 'email | required|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        //crate user
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user->save;
+
+        return response()->json($user, 201);
+    }
+}
